66cca652303f44598075e9435d9a2bd58a70fbb5
sean brittingha <seanbritt4@gmail.com>
2019-10-17 06:14:00 -0400

    dummy login

--- public/login.js
+++ public/login.js
@@ -1,18 +1,19 @@
 function user_login(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            xhttp.send("SEND", 'hello from login.js', true);
-        }
-    };
-    xhttp.open("GET", "todo.txt", true);
-    xhttp.send();
-//     var name = prompt("Username:", "spotify username");
-//     if(name == null || name == ''){
-//         alert("Please try again!");
-//     }else{
-//         var greeting = "Welcome, ".concat(name, '!');
-//         document.getElementById("greet").innerHTML = greeting;
-//     }
+    var name = prompt("Username:", "spotify username");
+    if(name == null || name == ''){
+        alert("Please try again!");
+    }else{
+        var greeting = "Welcome, ".concat(name, '!');
+        document.getElementById("greet").innerHTML = greeting;
+    }
+    
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         xhttp.send("SEND", 'hello from login.js', true);
+    //     }
+    // };
+    // xhttp.open("GET", "todo.txt", true);
+    // xhttp.send();
 
 }
