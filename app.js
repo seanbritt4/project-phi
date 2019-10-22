cae35cf0d2bdcb83d6ee461b7d6071ee8505bc82
sean brittingha <seanbritt4@gmail.com>
2019-10-21 23:25:08 -0400

    more small changes and tests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bparser = require('body-parser');
 
 var indexRouter = require('./routes/index');    //index.js
 var usersRouter = require('./routes/users');    //users.js
