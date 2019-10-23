dc50dd5aea1114580e0a566ce63dff38c66ae117
sean brittingha <seanbritt4@gmail.com>
2019-10-23 15:25:22 -0400

    reworking html

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// var jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM();
-// const { document } = (new JSDOM('')).window;
-// global.document = document;
-//
-// var $ = jQuery = require('jquery')(window);
-//
-// $.ajaxSetup({
-//     url: "index.hbs"
-// });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { name: "Guest", title: 'Project PHI' });
