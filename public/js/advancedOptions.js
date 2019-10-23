4676f26358413b40dc34960455a4114d2bb68d98
madams102 <33868107+madams102@users.noreply.github.com>
2019-10-23 01:15:07 -0400

    Add files via upload

---
+++ public/js/advancedOptions.js
@@ -0,0 +1,8 @@
+function toggleAdvanced() {
+	var x = document.getElementById("advanced");
+	if(x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
