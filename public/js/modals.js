3370453838ef0b59f3ce013e49ccf07f5ccdca23
madams102 <33868107+madams102@users.noreply.github.com>
2019-11-03 22:40:23 -0500

    Add files via upload

---
+++ public/js/modals.js
@@ -0,0 +1,27 @@
+/*about modal*/
+
+var modal, aboutBtn, aboutSpan;
+
+window.onload = function() {
+    aboutBtn = document.getElementById('about');
+    modal = document.getElementById('aboutModal');
+    aboutSpan = document.getElementsByClassName("close")[0];
+    aboutBtn.onclick = function(){
+        console.log('clicked about')
+        modal.style.display = "block";
+    }
+
+    // // When the user clicks on <span> (x), close the modal
+    // aboutSpan.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+}
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/*other modals ???*/
