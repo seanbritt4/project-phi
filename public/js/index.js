8f77b469e989430fc75aed45a396747b27ef835a
Blade Forsythe <bladeforsythe@gmail.com>
2019-12-02 23:06:59 -0500

    broke

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,37 @@
 $(document).ready(() => {
-  $('#input').hide()
-  $('#output').hide()
-  $('#about-content').hide()
+      $('#input').hide()
+      $('#output').hide()
+      $('#about-content').hide()
+      $('#input-header').hide()
+      $('#emoji-slider').hide()
+      $('#advanced-input').hide()
 
-  $('#about-button').on('click', () => {
-    $('#title').fadeToggle()
-    $('#title2').fadeToggle()
-    $('#about-info').fadeToggle()
-    $('#start-button').fadeToggle()
-  })
 
-  //
-  // // When the user clicks on <span> (x), close the modal
-  $('#close').on('click', () => {
-    $('#title').show()
-    $('#title2').show()
-    $('#start-button').show()
-  })
-})
+      $('#about-button').on('click', () => {
+        $('#title').fadeToggle()
+        $('#title2').fadeToggle()
+        $('#about-info').fadeToggle()
+        $('#start-button').fadeToggle()
+      })
 
-$('#start-button').on('click', () => {
-  window.open('views/submit.hbs', "_self")
-})
+      // // When the user clicks on <span> (x), close the modal
+      $('#close').on('click', () => {
+        $('#title').fadeToggle()
+        $('#title2').fadeToggle()
+        $('#start-button').fadeToggle()
+      })
+
+      $('#start-button').on('click', () => {
+        $('#title').hide()
+        $('#title2').hide()
+        $('#start-button').hide()
+        $('#input').show()
+
+      $('#advanced-button').on('click', () => {
+        $('#advanced-options').show()
+      })
+
+      $('#restart').on('click', () => {
+        window.location.reload()
+      })
+    })
