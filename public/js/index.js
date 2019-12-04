d193397057afa1a9e4b24c357c5f36f8693f96ff
Blade Forsythe <bladeforsythe@gmail.com>
2019-12-03 22:36:06 -0500

    getting closer

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,7 @@ $(document).ready(() => {
       $('#advanced-input').hide()
 
 
+      console.log("Hello")
       $('#about-button').on('click', () => {
         $('#title').fadeToggle()
         $('#title2').fadeToggle()
@@ -25,13 +26,52 @@ $(document).ready(() => {
         $('#title').hide()
         $('#title2').hide()
         $('#start-button').hide()
-        $('#input').show()
+        $('#navbar').hide()
+        $('#input').fadeToggle()
+        $('#input-header').fadeToggle()
+        $('#emoji-slider').fadeToggle()
+      })
 
       $('#advanced-button').on('click', () => {
         $('#advanced-options').show()
       })
 
+      $('#submit-button').on('click', () => {
+          // console.log('in submit');
+          //declares data variable, will be used to store data and pass it
+          //along to the server
+          var data = {};
+          data.user_values = 23; //stores vals in data
+          data.num_songs = 10;
+          $.ajax({
+            type: 'POST', //type of ajax call
+            data: JSON.stringify(data), //prepares for flight
+            contentType: 'application/json', //unsure...
+            url: '/', //
+            // url: JSON.stringify(process.env.PORT),     //destination, can use this more effectively
+            // url: 'http://localhost:3000',     //destination, can use this more effectively
+            success: function(data) { //on success, recv's data from server
+              console.log('success, send', JSON.stringify(data));
+              console.log(data);
+              // now we can do stuff with the data from the server
+              $("#playlist").append('<b>Your playlist:</b><br><ul>');
+              // for(var i in data.num_songs){
+              for (var i = 0; i < data.num_songs; i++) {
+                // $('#output').append('<br>', data.values[i]);
+                // $('#output').append(i, '<br>');
+                $('#playlist').append('<li>', i, '</li>');
+              }
+              $("#playlist").append('</ul>');
+            }
+          });
+
+          $('#input').hide();
+          $('#output').show();
+          $('#emoji-slider').hide();
+          $('#input-header').hide();
+        })
+
       $('#restart').on('click', () => {
         window.location.reload()
       })
-    })
+  })
