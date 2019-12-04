4e58807ffb32ecdd0571a062e85357ea010646da
Sean Brittingham <seanbritt4@gmail.com>
2019-12-04 15:35:10 -0500

    merged with two issues

--- public/js/index.js
+++ public/js/index.js
@@ -1,75 +1,75 @@
 $(document).ready(() => {
       $('#input').hide()
-      $('#output').hide()
+      // $('#output').hide()
       $('#about-content').hide()
       $('#input-header').hide()
       $('#emoji-slider').hide()
       $('#advanced-input').hide()
       $('#num-songs').hide()
 
-      console.log("Hello")
       $('#about-button').on('click', () => {
         $('#title').fadeToggle()
         $('#title2').fadeToggle()
         $('#about-info').fadeToggle()
         $('#start-button').fadeToggle()
       })
-
+      
       // // When the user clicks on <span> (x), close the modal
       $('#close').on('click', () => {
         $('#title').fadeToggle()
         $('#title2').fadeToggle()
         $('#start-button').fadeToggle()
       })
+      
+    $('#start-button').on('click', () => {
+      console.log("Hello")
+      $('#title').hide()
+      $('#title2').hide()
+      $('#start-button').hide()
+      $('#navbar').hide()
+      $('#input').fadeToggle()
+      $('#input-header').fadeToggle()
+      $('#emoji-slider').fadeToggle()
+      $('#num-songs').fadeToggle()
+    })
+      
+    $('#submit-button').on('click', () => {
+      var data = {};  //data to be sent to back end
+      var e = document.getElementById("num-songs");
+      data.num_songs = $("#num-songs :selected").val(); //get user selected numner of songs
+      e = document.getElementById("genre-form");
+      data.genre = $("#genre-form :selected").val();  //get user selected genre
+      data.user_values = emojiSubmit() //stores vals in data
+      //  console.log("Test: ", data.num_songs, " ", data.genre);
+      console.log(data)
+      
+      $.ajax({
+        type: 'POST', //type of ajax call
+        data: JSON.stringify(data), //prepares for flight
+        contentType: 'application/json', //unsure...
+        url: '/', //
 
-      $('#start-button').on('click', () => {
-        $('#title').hide()
-        $('#title2').hide()
-        $('#start-button').hide()
-        $('#navbar').hide()
-        $('#input').fadeToggle()
-        $('#input-header').fadeToggle()
-        $('#emoji-slider').fadeToggle()
-        $('#num-songs').fadeToggle()
-        
-         var data = {};
-         var e = document.getElementById("num-songs");
-         data.num_songs = $("#num-songs :selected").val();//e.options[e.selectedIndex].value;
-         e = document.getElementById("genre-form");
-         data.genre = $("#genre-form :selected").val();//e.options[e.selectedIndex].value;
-         data.user_values = 23; //stores vals in data
-         console.log("Test: ", data.num_songs, " ", data.genre);
-
-         $.ajax({
-           type: 'POST', //type of ajax call
-           data: JSON.stringify(data), //prepares for flight
-           contentType: 'application/json', //unsure...
-           url: '/', //
-           // url: JSON.stringify(process.env.PORT),     //destination, can use this more effectively
-           // url: 'http://localhost:3000',     //destination, can use this more effectively
-           success: function(data) { //on success, recv's data from server
-             console.log('success, send', JSON.stringify(data));
-             console.log(data);
-             // now we can do stuff with the data from the server
-             $("#playlist").append('<b>Your playlist:</b><br><ul>');
-             // for(var i in data.num_songs){
-             for (var i = 0; i < data.num_songs; i++) {
-               // $('#output').append('<br>', data.values[i]);
-               // $('#output').append(i, '<br>');
-               $('#playlist').append('<li>', i, '</li>');
-             }
-             $("#playlist").append('</ul>');
-           }
-         });
-
-          $('#input').hide();
-          $('#output').show();
-          $('#emoji-slider').hide();
-          $('#num-songs').hide();
-          $('#input-header').hide();
-        })
+        success: function(data) { //on success, recv's data from server
+        // now we can do stuff with the data from the server
+        $("#playlist").append('<b>Your playlist:</b><br><ul>');
+        // for(var i in data.num_songs){
+          for (var i = 0; i < data.num_songs; i++) {
+            // $('#output').append('<br>', data.values[i]);
+            // $('#output').append(i, '<br>');
+            $('#playlist').append('<li>', /* i,*/'</li>');
+          }
+          $("#playlist").append('</ul>');
+        }
+      });
+      
+      $('#input').hide();
+      $('#emoji-slider').hide();
+      $('#num-songs').hide();
+      $('#input-header').hide();
+      // $('#output').show();
+    })
 
     $('#restart').on('click', () => {
         window.location.reload()
-      })
+    })
   })
