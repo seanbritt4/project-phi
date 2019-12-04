f51f8be6173baa000bd0cf9a299ab4c1903e0323
silveranzie <47569479+silveranzie@users.noreply.github.com>
2019-12-04 12:11:48 -0500

    query works

--- phi_modules/database/queries.js
+++ phi_modules/database/queries.js
@@ -27,6 +27,7 @@ exports.test = function(msg){
 //butter
 // exports.createQuery = function(query, content){
 // }
+/*
 let statement = `SELECT artist.artist_name, album.album_name, track.track_name, audio_features.*
 FROM artist
 JOIN album
@@ -44,5 +45,17 @@ AND (loudness BETWEEN ${minLd} AND ${maxLd})
 AND (speechiness BETWEEN ${min} AND ${max})
 AND (tempo BETWEEN ${mintmp} AND ${maxtmp})
 AND (valence BETWEEN ${minval} AND ${maxval})
+ORDER BY RAND()
+LIMIT 1000;`;
+*/
+let statement = `SELECT artist.artist_name, album.album_name, track.track_name, audio_features.*
+FROM artist
+JOIN album
+ON artist.artist_id=album.artist_id
+JOIN track
+ON album.album_id=track.album_id
+JOIN audio_features
+ON track.track_id=audio_features.track_id
+WHERE 1=1
 ORDER BY RAND()
 LIMIT 1000;`;
